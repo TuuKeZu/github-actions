a88f913598d24fe7db2dfaad508b971ee5f4af4e
TuuKeZu <moilanentuukka5@gmail.com>
2023-01-22 16:01:19 +0200

    Added self-updater

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use rustdate::{update::UpdateBuilder, utility::OsType};
+
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-fn main() {
+type Error = Box<dyn std::error::Error>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    UpdateBuilder::new()
+        .set_verbal(true)
+        .set_github_user("TuuKeZu")
+        .set_github_repo("github-actions")
+        .set_binary_path(OsType::Windows, "x86_64-pc-windows-gnu.zip")
+        .set_binary_path(OsType::Linux, "x86_64-apple-darwin.zip")
+        .check_for_updates()
+        .await?;
+
     println!("Hello.rs v{VERSION}");
     println!("-------------------");
     println!("> Hello, world!");
@@ -9,4 +23,6 @@ fn main() {
     // terminal shouldn't terminate on close
     println!("Press any key to continue...");
     std::io::stdin().read_line(&mut String::new()).unwrap();
+
+    Ok(())
 }
